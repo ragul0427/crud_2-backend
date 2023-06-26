29409211550fbe33c7639b90e77dfa77027a87f5
ragul0427 <ragulhp2704@gmail.com>
2023-06-26 12:19:12 +0530

    rgkf

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 var express = require("express")
 var app=express()
-
 var bodyParser = require("body-parser")
 var cors = require("cors")
 const mongoose = require("mongoose")
-
 app.use(bodyParser.json())
-
 app.use(cors())
-
 const todo = require("./routes/todoRoutes")
-
 app.use('/api/todos',todo)
 
 
 const mongoUri = "mongodb+srv://ragul8523947145:ragulhp2704@todo.xgarceo.mongodb.net/todoApp?retryWrites=true&w=majority"
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { console.log("mongodb connected") }).catch((err) => {
     console.log(err)
+})
+
+app.get('/', (req, res) => {
+    res.send({"hello"})
 })
 
 const port = 4000
